54d5bad88e5a334d199e351ae16f873b611c3f37
B S Karan Kumar (CW) <Bskaran.Kumar@jda.com>
2022-05-17 09:37:22 +0530

    First Commit

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomizedTables from './CustomizedTables';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomizedTables/>
+    </div>
+  );
+}
+
+export default App;
